c5f38258342181ef266df777e7a00423e4080ecd
lobos1995 <entherius@gmail.com>
2019-10-28 14:49:16 -0300

    cambio de sintaxis a terraform 12

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_cognito_identity_pool" "main" {
-  identity_pool_name               = "${var.identity_pool_name}"
-  allow_unauthenticated_identities = "${var.allow_unauthenticated_identities}"
+  identity_pool_name               = var.identity_pool_name
+  allow_unauthenticated_identities = var.allow_unauthenticated_identities
   cognito_identity_providers {
-    client_id               = "${var.client_id}"
-    provider_name           = "${var.provider_name}"
-    server_side_token_check = "${var.server_side_token_check}"
+    client_id               = var.client_id
+    provider_name           = var.provider_name
+    server_side_token_check = var.server_side_token_check
   }
-  supported_login_providers    = "${var.supported_login_providers}"
-  saml_provider_arns           = ["${var.saml_provider_arns}"]
-  openid_connect_provider_arns = ["${var.openid_connect_provider_arns}"]
+  supported_login_providers    = var.supported_login_providers
+  saml_provider_arns           = var.saml_provider_arns
+  openid_connect_provider_arns = var.openid_connect_provider_arns
 }
--- variables.tf
+++ variables.tf
@@ -1,39 +1,39 @@
 variable "identity_pool_name" {
-  type        = "string"
+  type        = string
   description = "The cognito identity pool name"
 }
 
 variable "allow_unauthenticated_identities" {
-  type        = "string"
+  type        = bool
   description = "Whether the identity pool supports unauthenticated logins or not.(true or false)"
 }
 
 variable "developer_provider_name" {
-  type        = "string"
+  type        = string
   description = "The domain by which Cognito will refer to your users. This name acts as a placeholder that allows your backend and the Cognito service to communicate about the developer provider."
   default     = ""
 }
 
 variable "cognito_identity_providers" {
-  type        = "map"
+  type        = map
   description = "An array of Amazon Cognito Identity user pools and their client IDs."
   default     = {}
 }
 
 variable "openid_connect_provider_arns" {
-  type        = "list"
+  type        = list
   description = "A list of OpendID Connect provider ARNs."
   default     = []
 }
 
 variable "saml_provider_arns" {
-  type        = "list"
+  type        = list
   description = "An array of Amazon Resource Names (ARNs) of the SAML provider for your identity."
   default     = []
 }
 
 variable "supported_login_providers" {
-  type        = "map"
+  type        = map
   description = "Key-Value pairs mapping provider names to provider app IDs."
   default     = {}
 }
@@ -41,20 +41,20 @@ variable "supported_login_providers" {
 ###Cognito Identity Providers
 
 variable "client_id" {
-  type        = "string"
+  type        = string
   description = "The client ID for the Amazon Cognito Identity User Pool."
   default     = ""
 }
 
 variable "provider_name" {
-  type        = "string"
+  type        = string
   description = "The provider name for an Amazon Cognito Identity User Pool."
   default     = ""
 }
 
 variable "server_side_token_check" {
-  type        = "string"
+  type        = bool
   description = "Whether server-side token validation is enabled for the identity provider’s token or not.(true or false)" 
-  default     = ""
+  default     = false
 }
 
