233e8461872e0f27c8ce176e40f763a0e00c9802
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-06-29 14:14:56 -0300

    new module upgrade tf12

--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
 output "id" {
   description = "An identity pool ID in the format"
-  value       = "${aws_cognito_identity_pool.main.id}"
+  value       = aws_cognito_identity_pool.main.id
 }
+
--- variables.tf
+++ variables.tf
@@ -1,39 +1,39 @@
 variable "identity_pool_name" {
-  type        = "string"
+  type        = string
   description = "The cognito identity pool name"
 }
 
 variable "allow_unauthenticated_identities" {
-  type        = "string"
+  type        = string
   description = "Whether the identity pool supports unauthenticated logins or not.(true or false)"
 }
 
 variable "developer_provider_name" {
-  type        = "string"
+  type        = string
   description = "The domain by which Cognito will refer to your users. This name acts as a placeholder that allows your backend and the Cognito service to communicate about the developer provider."
   default     = ""
 }
 
 variable "cognito_identity_providers" {
-  type        = "map"
+  type        = map(string)
   description = "An array of Amazon Cognito Identity user pools and their client IDs."
   default     = {}
 }
 
 variable "openid_connect_provider_arns" {
-  type        = "list"
+  type        = list(string)
   description = "A list of OpendID Connect provider ARNs."
   default     = []
 }
 
 variable "saml_provider_arns" {
-  type        = "list"
+  type        = list(string)
   description = "An array of Amazon Resource Names (ARNs) of the SAML provider for your identity."
   default     = []
 }
 
 variable "supported_login_providers" {
-  type        = "map"
+  type        = map(string)
   description = "Key-Value pairs mapping provider names to provider app IDs."
   default     = {}
 }
@@ -41,20 +41,20 @@ variable "supported_login_providers" {
 ###Cognito Identity Providers
 
 variable "client_id" {
-  type        = "string"
+  type        = string
   description = "The client ID for the Amazon Cognito Identity User Pool."
   default     = ""
 }
 
 variable "provider_name" {
-  type        = "string"
+  type        = string
   description = "The provider name for an Amazon Cognito Identity User Pool."
   default     = ""
 }
 
 variable "server_side_token_check" {
-  type        = "string"
-  description = "Whether server-side token validation is enabled for the identity provider’s token or not.(true or false)" 
+  type        = string
+  description = "Whether server-side token validation is enabled for the identity provider’s token or not.(true or false)"
   default     = ""
 }
 
